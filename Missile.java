f7b94db1ad1767bac58577299fd666baa05f7027
SAC-CS112-Mier-Caesar <caesarmier@yahoo.com>
2016-06-01 21:02:08 -0700

    Create Missile.java

---
+++ Missile.java
@@ -0,0 +1,36 @@
+
+public class Missile implements FlyInterface{
+
+	private int speed;
+	private int elevationPM;
+	public Missile(int speed, int elevationPM) {
+		this.speed=speed;
+		this.elevationPM=elevationPM;
+	}
+
+	@Override
+	public void setAirSpeed(int airSpeed) {
+		this.speed=airSpeed;
+		
+	}
+
+	@Override
+	public void setAltiMeter(int elevationPM) {
+		this.elevationPM=elevationPM;
+		
+	}
+
+	@Override
+	public void fly() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public int getAirSpeed(){
+		return speed;
+	}
+	
+	public int getAltiMeter(){
+		return elevationPM;
+	}
+}
