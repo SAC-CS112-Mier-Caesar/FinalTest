c64af1f437d44b95c9f38ed060c325de4970df29
SAC-CS112-Mier-Caesar <caesarmier@yahoo.com>
2016-06-01 21:01:01 -0700

    Create Bird.java

---
+++ Bird.java
@@ -0,0 +1,40 @@
+
+public class Bird implements FlyInterface {
+
+	private int speed;
+	private int elevationPM;
+	
+	public Bird(int speed, int elevationPM) {
+		this.speed = speed;
+		this.elevationPM = elevationPM;
+	}
+
+	@Override
+	public void setAirSpeed(int airSpeed) {
+		this.speed=airSpeed;
+		
+	}
+
+	@Override
+	public void setAltiMeter(int elevationPM) {
+		this.elevationPM=elevationPM;
+		
+	}
+
+	@Override
+	public void fly() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public int getAirSpeed(){
+		return speed;
+	}
+	
+	public int getAltiMeter(){
+		return elevationPM;
+	}
+
+	
+
+}
