2f074b7ff837e1fae427db98feb7b85bc499a8b5
SAC-CS112-Mier-Caesar <caesarmier@yahoo.com>
2016-06-01 22:11:32 -0700

    Update Superman.java

--- Superman.java
+++ Superman.java
@@ -1,36 +1,18 @@
 
-public class Superman implements FlyInterface{
+public class Superman extends FlyClass{
 
-	private int speed;
-	private int elevationPM;
-	
-	public Superman(int speed, int elevationPM){
-		this.speed = speed;
-		this.elevationPM = elevationPM;
-	}
-	@Override
-	public void setAirSpeed(int airSpeed) {
-		this.speed = airSpeed;
+public Superman(int x, int y) {
 		
-	}
+		super(x,y);
 	
-	public int getAirSpeed(){
-		return speed;
 	}
 
-	@Override
-	public void setAltiMeter(int elevationPM) {
-		this.elevationPM=elevationPM;
-		
+	public int getSpeed(){
+		return super.getSpeed();
 	}
 	
-	public int getAltiMeter(){
-		return elevationPM;
-	}
-
-	@Override
-	public void fly() {
-		
+	public int getAltMeter(){
+		return super.getAltMeter();
 		
 	}
 	
