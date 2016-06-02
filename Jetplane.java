63ac2971ccec07ed930acb96cc2ac5b9594d0a5c
SAC-CS112-Mier-Caesar <caesarmier@yahoo.com>
2016-06-01 21:01:44 -0700

    Create Jetplane.java

---
+++ Jetplane.java
@@ -0,0 +1,39 @@
+
+public class Jetplane implements FlyInterface {
+
+	private int speed;
+	private int elevationPM;
+	
+	public Jetplane(int speed, int elevationPM) {
+
+		this.speed=speed;
+		this.elevationPM=elevationPM;
+	}
+
+	@Override
+	public void setAirSpeed(int airSpeed) {
+		this.speed=airSpeed;
+		
+	}
+
+	@Override
+	public void setAltiMeter(int elevationPM) {
+		this.elevationPM=elevationPM;
+		
+	}
+
+	@Override
+	public void fly() {
+		// TODO Auto-generated method stub
+		
+	}
+	public int getAirSpeed(){
+		return speed;
+	}
+	
+	public int getAltiMeter(){
+		return elevationPM;
+	}
+
+
+}
